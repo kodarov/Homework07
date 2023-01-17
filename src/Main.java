dc9688aaa225135cf2f9146a2dfe833867748940
kodarov_salavat <kodarov@gmail.com>
2023-01-17 23:00:23 +0400

    2st commit reset

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ public class Main {
     }
     public static void task1() {
         System.out.println("Задача 1");
-    }
+        }
+
     public static void task2() {
         System.out.println("Задача 2");
     }
