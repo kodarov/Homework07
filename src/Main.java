c435aea588855726317e65341c1d7e5ff716d7b8
kodarov_salavat <kodarov@gmail.com>
2023-01-17 23:01:42 +0400

    HW7/task01

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,43 @@ public class Main {
         task7();
         task8();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
+        double deposit = 0; // вклад
+        double payment = 15_000; // накопления ежемесячные
+        int month = 0; // порядковый номер месяца
+        while (deposit < 2_459_000) {
+            deposit = deposit + payment;
+            month++;
+            System.out.printf("Месяц %d сумма накоплений равна %.2f \n", month, deposit);
         }
+    }
 
     public static void task2() {
         System.out.println("Задача 2");
     }
+
     public static void task3() {
         System.out.println("Задача 3");
     }
+
     public static void task4() {
         System.out.println("Задача 4");
     }
+
     public static void task5() {
         System.out.println("Задача 5");
     }
+
     public static void task6() {
         System.out.println("Задача 6");
     }
+
     public static void task7() {
         System.out.println("Задача 7");
     }
+
     public static void task8() {
         System.out.println("Задача 8");
     }
